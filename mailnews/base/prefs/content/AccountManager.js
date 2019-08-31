016fbc6ab4836dc0cd29e4084e0dc750d31d5f80
Paul Morris <paul@thunderbird.net>
2019-08-30 22:28:12 -0400

    Bug 1578477 - Fix eslint "no-useless-concat" errors in mailnews/. r=mkmelin
    Ported from bug 1577835.

--- mailnews/base/prefs/content/AccountManager.js
+++ mailnews/base/prefs/content/AccountManager.js
@@ -1846,7 +1846,7 @@ var gAccountTree = {
       if (server) {
         treecell.setAttribute(
           "properties",
-          "folderNameCol isServer-true" + " serverType-" + server.type
+          "folderNameCol isServer-true serverType-" + server.type
         );
         // For IM accounts, we can try to fetch a protocol specific icon.
         if (server.type == "im") {
