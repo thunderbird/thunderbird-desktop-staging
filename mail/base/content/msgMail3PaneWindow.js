5df09475320ea17029f2f70f2911dcb5173f9d10
Magnus Melin <mkmelin+mozilla@iki.fi>
2021-01-17 12:27:55 +0200

    Bug 1687034 - hide both Global Search icon and input when Global Search disabled. r=Paenglab a=wsmwk

--- mail/base/content/msgMail3PaneWindow.js
+++ mail/base/content/msgMail3PaneWindow.js
@@ -682,6 +682,22 @@ function OnLoadMessenger() {
     document.getElementById("multimessage")
   );
 
+  // Depending on the pref, hide/show the gloda toolbar search widgets.
+  XPCOMUtils.defineLazyPreferenceGetter(
+    this,
+    "gGlodaEnabled",
+    "mailnews.database.global.indexer.enabled",
+    true,
+    (pref, oldVal, newVal) => {
+      for (let widget of document.querySelectorAll(".gloda-search-widget")) {
+        widget.hidden = !newVal;
+      }
+    }
+  );
+  for (let widget of document.querySelectorAll(".gloda-search-widget")) {
+    widget.hidden = !this.gGlodaEnabled;
+  }
+
   window.addEventListener("AppCommand", HandleAppCommandEvent, true);
 
   // Set up the appmenus. (This has to happen after the DOM has loaded.)
