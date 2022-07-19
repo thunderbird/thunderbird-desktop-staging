5363f6bd47aa2ef08f44f5f6f5d3ce1757922b87
Kai Engert <kaie@kuix.de>
2022-07-19 20:44:08 +1000

    Bug 1775115 - Optimize automatic processing of OpenPGP public key atttachments, avoid quadratic complexity. r=mkmelin a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D152079

--- mail/extensions/openpgp/content/ui/enigmailMessengerOverlay.js
+++ mail/extensions/openpgp/content/ui/enigmailMessengerOverlay.js
@@ -2842,7 +2842,7 @@ Enigmail.msg = {
       errorMsgObj,
       true,
       true,
-      false
+      true
     );
 
     // If we cannot analyze the keyblock, or if it's empty, or if we
@@ -2854,12 +2854,6 @@ Enigmail.msg = {
     this.fetchParticipants();
 
     for (let newKey of preview) {
-      let keyDataOneKey = await RNP.getOnePubKeyFromKeyBlock(
-        keyData,
-        newKey.fpr,
-        false
-      );
-
       let oldKey = EnigmailKeyRing.getKeyById(newKey.fpr);
       if (!oldKey) {
         // If the key is unknown, an expired key cannot help us
@@ -2874,7 +2868,7 @@ Enigmail.msg = {
           let db = await CollectedKeysDB.getInstance();
           let existing = await db.findKeyForFingerprint(newKey.fpr);
           if (existing) {
-            let key = await db.mergeExisting(newKey, keyDataOneKey, {
+            let key = await db.mergeExisting(newKey, newKey.pubKey, {
               uri: `mid:${gMessageDisplay.displayedMessage.messageId}`,
               type: isBinaryAutocrypt ? "autocrypt" : "attachment",
               description,
@@ -2931,7 +2925,7 @@ Enigmail.msg = {
             binary: isBinaryAutocrypt,
           };
           Enigmail.msg.attachedSenderEmailKeysIndex.push(info);
-          Enigmail.msg.attachedKeys.push(keyDataOneKey);
+          Enigmail.msg.attachedKeys.push(newKey.pubKey);
         }
 
         // We want to collect keys for potential later use, however,
@@ -2972,8 +2966,8 @@ Enigmail.msg = {
             candidate.skip = false;
             candidate.newKeyObj = newKey;
             candidate.pubKey = isBinaryAutocrypt
-              ? RNP.enArmorString(keyDataOneKey, "public key")
-              : keyDataOneKey;
+              ? RNP.enArmorString(newKey.pubKey, "public key")
+              : newKey.pubKey;
             candidate.source = {
               uri: `mid:${gMessageDisplay.displayedMessage.messageId}`,
               type: isBinaryAutocrypt ? "autocrypt" : "attachment",
@@ -3011,7 +3005,7 @@ Enigmail.msg = {
       if (
         !(await EnigmailKeyRing.importKeyDataSilent(
           window,
-          keyDataOneKey,
+          newKey.pubKey,
           isBinaryAutocrypt,
           "0x" + newKey.fpr
         ))
