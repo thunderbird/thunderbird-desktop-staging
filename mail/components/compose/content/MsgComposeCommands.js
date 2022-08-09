ad4aba772d6b9c0bc29f7f0bb73e1e40736d914a
Richard Marti <richard.marti@gmail.com>
2022-08-09 20:19:43 +0000

    Bug 1783001 - Don't close the spellcheck menu on select/deselect a language. r=aleca  a=wsmwk
    Differential Revision: https://phabricator.services.mozilla.com/D153909

--- mail/components/compose/content/MsgComposeCommands.js
+++ mail/components/compose/content/MsgComposeCommands.js
@@ -6857,6 +6857,9 @@ function InitLanguageMenu() {
       item.setAttribute("value", dict.localeCode);
       item.setAttribute("type", "checkbox");
       item.setAttribute("selection-type", "multiple");
+      if (dictList.length > 1) {
+        item.setAttribute("closemenu", "none");
+      }
       return item;
     }),
     ...getMoreArray
