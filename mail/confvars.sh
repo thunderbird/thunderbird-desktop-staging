7d902a38b7d70ada17d026f09a63ee4384fc9db7
Daniel Darnell <daniel@thunderbird.net>
2025-08-07 19:03:25 -0700

    Bug 1981291 - [beta] Update official branding directory during merges. r=coreycb a=coreycb
    Differential Revision: https://phabricator.services.mozilla.com/D260408

--- mail/confvars.sh
+++ mail/confvars.sh
@@ -5,5 +5,6 @@
 
 MOZ_BRANDING_DIRECTORY=comm/mail/branding/nightly
 
-# This is a temporary hard-coded measure to accomodate MSIX repackaging
+# This line is present (but intentionally unset) in comm-central to accommodate
+# merge automation branding updates for MSIX repackaging.
 MOZ_OFFICIAL_BRANDING_DIRECTORY=comm/mail/branding/tb_beta
